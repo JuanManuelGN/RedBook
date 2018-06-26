cd1d65e78fab8181d66ade88ed79925dc84187d9
DIR\j.garcia.navarro <juanmanuel.gn@gmail.com>
2018-06-26 16:56:54 +0200

    List length with foldRight. Exercise 3.9

--- src/main/scala/redBook/part1introduction/bFunctionalDataStructures/Exercise39_FoldR.scala
+++ src/main/scala/redBook/part1introduction/bFunctionalDataStructures/Exercise39_FoldR.scala
@@ -1,10 +1,7 @@
 package redBook.part1introduction.bFunctionalDataStructures
 
 /**
-  * Not everything works out so nicely. Implement a function, init, that returns a List
-  * consisting of all but the last element of a List. So, given List(1,2,3,4), init will
-  * return List(1,2,3). Why can’t this function be implemented in constant time like
-  * tail?
+  * Compute the length of a list using foldRight.
   */
 class FoldR {
   def length[A](as: List[A]): Int = List.foldRight(as,0)((a,acc) => acc + 1)
