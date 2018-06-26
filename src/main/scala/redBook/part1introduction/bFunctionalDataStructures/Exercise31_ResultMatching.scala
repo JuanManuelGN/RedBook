05eaa827074e264145b071cbfdace0d8ac2fef6a
DIR\j.garcia.navarro <juanmanuel.gn@gmail.com>
2018-06-26 13:09:11 +0200

    Pattern Matching. Exercise 3.1

---
+++ src/main/scala/redBook/part1introduction/bFunctionalDataStructures/Exercise31_ResultMatching.scala
@@ -0,0 +1,11 @@
+package redBook.part1introduction.bFunctionalDataStructures
+
+class ResultMatching {
+  val r = List.x
+}
+object ResultMatching extends App {
+  val rm : ResultMatching = new ResultMatching
+  println(
+    rm.r
+  )
+}
