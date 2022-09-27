d08c56d24ba95a5418a9d3f5a7adb87239f14a75
Juan Manuel García Navarro <juan.garcia@47deg.com>
2022-09-27 13:51:55 +0200

    exercise 4.1 Option

---
+++ src/main/scala/part_1_introduction/c_handling_errors_without_exceptions/Option.scala
@@ -0,0 +1,34 @@
+package part_1_introduction.c_handling_errors_without_exceptions
+
+import scala.{Option => _, Either => _, _}
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(_) => _
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case None => None
+    case Some(a) => if (f(a)) this else None
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+
+case object None extends Option[Nothing]
