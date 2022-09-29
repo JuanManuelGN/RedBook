77584b7d2b9f46867306b462ae9e7b209a943449
Juan Manuel García Navarro <juan.garcia@47deg.com>
2022-09-29 18:49:08 +0200

    exercise 4.4 sequence

--- src/main/scala/part_1_introduction/c_handling_errors_without_exceptions/Option.scala
+++ src/main/scala/part_1_introduction/c_handling_errors_without_exceptions/Option.scala
@@ -1,6 +1,8 @@
 package part_1_introduction.c_handling_errors_without_exceptions
 
-import scala.{Option => _, Either => _, _}
+import part_1_introduction.c_handling_errors_without_exceptions.Map2.map2
+
+import scala.{::, Either => _, Option => _, _}
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
@@ -47,6 +49,16 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] = {
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
   }
+
+  /**
+   * List(Option(2),None,Option(5)) = None
+   * List(Option(1),Option(2),Option(3)) = Option(List(1,2,3))
+   * @param xs
+   * @tparam A
+   * @return
+   */
+  def sequence[A](xs: List[Option[A]]): Option[List[A]] =
+    xs.foldRight[Option[List[A]]](Some(Nil))((a,b) => map2(a,b)(_ :: _))
 }
 
 object Variance extends App {
@@ -67,5 +79,18 @@ object Map2 extends Option[String] with App {
   println(
     result
   )
+}
 
+object Sequence extends App {
+  val input = List(Some(1), Some(2), Some(3))
+  val result = Option.sequence(input)
+  println(
+    result
+  )
+
+  val inputError = List(Some(1), None, Some(3))
+  val resultError = Option.sequence(inputError)
+  println(
+    resultError
+  )
 }
