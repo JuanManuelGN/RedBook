d6aa5beb4d2a8499dffbd30992060d429ee5a8df
Juan Manuel García Navarro <juan.garcia@47deg.com>
2022-09-30 12:52:02 +0200

    exercise 4.6 Either

--- src/main/scala/part_1_introduction/c_handling_errors_without_exceptions/Either.scala
+++ src/main/scala/part_1_introduction/c_handling_errors_without_exceptions/Either.scala
@@ -3,11 +3,20 @@ package part_1_introduction.c_handling_errors_without_exceptions
 import scala.{Either => _, Option => _}
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(v) => Right(f(v))
+  }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(v) => f(v)
+  }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(v) => this
+  }
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
 }
@@ -15,3 +24,38 @@ sealed trait Either[+E, +A] {
 case class Left[+E](value: E) extends Either[E, Nothing]
 
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object EitherT extends App {
+  val eitherOk: Either[Exception, Int] = Right(5)
+  val eitherKo: Either[Exception, Int] = Left(new Exception("error"))
+
+  val mapOk = eitherOk.map(_ * 4)
+  val mapKo = eitherKo.map(_ * 4)
+
+  println(
+    mapOk
+  )
+  println(
+    mapKo
+  )
+
+  val eitherFlatMapKo = eitherKo.flatMap(x => Right(x.toString))
+  val eitherFlatMapOk = eitherOk.flatMap(x => Right(x.toString))
+
+  println(
+    eitherFlatMapOk
+  )
+  println(
+    eitherFlatMapKo
+  )
+
+  val eitherOrElseOk = eitherOk.orElse(Right(100))
+  val eitherOrElseKo = eitherKo.orElse(Right(100))
+
+  println(
+    eitherOrElseOk
+  )
+  println(
+    eitherOrElseKo
+  )
+}
