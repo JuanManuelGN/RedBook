54d9efd31ce62c6700e02dddc9649503784efc41
Juan Manuel García Navarro <juan.garcia@47deg.com>
2022-10-27 16:50:44 +0200

    Exercise 6.3
    Signed-off-by: Juan Manuel García Navarro <juan.garcia@47deg.com>

--- src/test/scala/part_1_introduction/e_purely_functional_state/FunctionalStateSpec.scala
+++ src/test/scala/part_1_introduction/e_purely_functional_state/FunctionalStateSpec.scala
@@ -2,21 +2,29 @@ package part_1_introduction.e_purely_functional_state
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import part_1_introduction.e_purely_functional_state.RNG.double
+import part_1_introduction.e_purely_functional_state.RNG.{double, intDouble}
 
 class FunctionalStateSpec extends AnyFlatSpec with should.Matchers {
 
+  val s = RNG.Simple(4)
+
   "Non negative random Int" should "Random number between 0 and Int.MaxValue" in {
 
-    val (i,_) = RNG.Simple(4).nextInt
+    val (i,_) = s.nextInt
     i should be >= 0
     i should be < Int.MaxValue
   }
 
   "double" should "Random number between 0 and 1, not including 1" in {
-    val (i, _) = double(RNG.Simple(4))
+    val (i, _) = double(s)
     i should be >= 0.0
     i should be < 1.0
+  }
+
+  "intDouble" should "couple of random, one Int and another Double" in {
+    val ((i, d),_) = intDouble(s)
+    i should be(1538995)
+    d should be(7.166503928601742E-4)
   }
 
 }
